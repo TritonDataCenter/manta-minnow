3104a585aada261b0377b6c0db2055306d45387d
Mark Cavage <mark.cavage@joyent.com>
2012-06-18 01:27:51 +0000

    MANTA-18: Initial commit of minnow agent

---
+++ Makefile
@@ -0,0 +1,92 @@
+#
+# Copyright (c) 2012, Joyent, Inc. All rights reserved.
+#
+# Makefile: basic Makefile for template API service
+#
+# This Makefile is a template for new repos. It contains only repo-specific
+# logic and uses included makefiles to supply common targets (javascriptlint,
+# jsstyle, restdown, etc.), which are used by other repos as well. You may well
+# need to rewrite most of this file, but you shouldn't need to touch the
+# included makefiles.
+#
+# If you find yourself adding support for new targets that could be useful for
+# other projects too, you should add these to the original versions of the
+# included Makefiles (in eng.git) so that other teams can use them too.
+#
+
+#
+# Files
+#
+DOC_FILES	 = index.restdown
+JS_FILES	:= $(shell ls *.js) $(shell find lib test -name '*.js')
+JSL_CONF_NODE	 = tools/jsl.node.conf
+JSL_FILES_NODE   = $(JS_FILES)
+JSSTYLE_FILES	 = $(JS_FILES)
+JSSTYLE_FLAGS    = -f tools/jsstyle.conf
+SMF_MANIFESTS_IN = smf/manifests/minnow.xml.in
+
+#
+# Variables
+#
+
+NAME 			= minnow
+PATH			:= $(NODE_INSTALL)/bin:${PATH}
+NODE_PREBUILT_VERSION	:= v0.6.19
+
+include ./tools/mk/Makefile.defs
+include ./tools/mk/Makefile.node_prebuilt.defs
+include ./tools/mk/Makefile.node_deps.defs
+include ./tools/mk/Makefile.smf.defs
+
+#
+# MG Variables
+#
+
+RELEASE_TARBALL         := $(NAME)-pkg-$(STAMP).tar.bz2
+ROOT                    := $(shell pwd)
+TMPDIR                  := /tmp/$(STAMP)
+
+#
+# Repo-specific targets
+#
+.PHONY: all
+all: $(SMF_MANIFESTS) | $(NPM_EXEC) $(REPO_DEPS)
+	$(NPM) install
+
+.PHONY: test
+test:
+	@echo "No tests"
+
+.PHONY: release
+release: all docs $(SMF_MANIFESTS)
+	@echo "Building $(RELEASE_TARBALL)"
+	@mkdir -p $(TMPDIR)/root/opt/smartdc/$(NAME)
+	@mkdir -p $(TMPDIR)/site
+	@touch $(TMPDIR)/site/.do-not-delete-me
+	@mkdir -p $(TMPDIR)/root
+	@mkdir -p $(TMPDIR)/root/opt/smartdc/$(NAME)/etc
+	cp -r   $(ROOT)/build \
+		$(ROOT)/main.js \
+		$(ROOT)/node_modules \
+		$(ROOT)/package.json \
+		$(ROOT)/smf \
+		$(TMPDIR)/root/opt/smartdc/$(NAME)
+	(cd $(TMPDIR) && $(TAR) -jcf $(ROOT)/$(RELEASE_TARBALL) root site)
+	@rm -rf $(TMPDIR)
+
+
+.PHONY: publish
+publish: release
+	@if [[ -z "$(BITS_DIR)" ]]; then \
+		@echo "error: 'BITS_DIR' must be set for 'publish' target"; \
+		exit 1; \
+	fi
+	mkdir -p $(BITS_DIR)/$(NAME)
+	cp $(ROOT)/$(RELEASE_TARBALL) $(BITS_DIR)/$(NAME)/$(RELEASE_TARBALL)
+
+
+include ./tools/mk/Makefile.deps
+include ./tools/mk/Makefile.node_prebuilt.targ
+include ./tools/mk/Makefile.node_deps.targ
+include ./tools/mk/Makefile.smf.targ
+include ./tools/mk/Makefile.targ
---
+++ main.js
@@ -0,0 +1,169 @@
+// Copyright (c) 2012, Joyent, Inc. All rights reserved.
+
+var assert = require('assert');
+var fs = require('fs');
+var os = require('os');
+
+var bunyan = require('bunyan');
+var moray = require('moray-client');
+var optimist = require('optimist');
+var statvfs = require('statvfs');
+var vasnyc = require('vasync');
+
+
+
+///--- Globals
+
+var ARGV = optimist.options({
+        'd': {
+                alias: 'debug',
+                describe: 'debug level'
+        },
+        'f': {
+                alias: 'file',
+                describe: 'configuration file',
+                demand: true
+        }
+}).argv;
+
+var LOG = bunyan.createLogger({
+        level: ARGV.d ? (ARGV.d > 1 ? 'trace' : 'debug') : 'info',
+        name: 'muppet',
+        serializers: {
+                err: bunyan.stdSerializers.err
+        },
+        src: ARGV.d ? true : false,
+        stream: process.stdout
+});
+
+
+
+///--- Internal Functions
+
+function errorAndExit(err, msg) {
+        LOG.fatal({err: err}, msg);
+        process.exit(1);
+}
+
+
+function readConfig() {
+        var cfg;
+        var file;
+
+        try {
+                file = fs.readFileSync(ARGV.f, 'utf8');
+        } catch (e) {
+                errorAndExit(e, 'unable to read %s', ARGV.f);
+        }
+
+        try {
+                cfg = JSON.parse(file);
+        } catch (e) {
+                errorAndExit(e, 'invalid JSON in %s', ARGV.f);
+        }
+
+        return (cfg);
+}
+
+
+function stat(filesystem, callback) {
+        statvfs(filesystem, function (err, s) {
+                if (err)
+                        return (callback(err));
+
+                var free = s.bavail * s.frsize;
+                var total = s.blocks * s.frsize;
+                var used = total - free;
+
+                var obj = {
+                        availableMB: Math.floor(free / 1048576),
+                        percentUsed: Math.ceil((used / total) * 100),
+                        filesystem: filesystem,
+                        statvfs: s
+                };
+
+                return (callback(null, obj));
+        });
+}
+
+
+function run(opts) {
+        var client = opts.client;
+        var key = os.hostname() + '.' + opts.domain;
+        var status;
+
+
+        vasnyc.pipeline({ funcs: [
+                function statfs(_, cb) {
+                        stat(opts.objectRoot, function (err, s) {
+                                if (err)
+                                        return (cb(err));
+
+                                s.hostname = key;
+                                status = s;
+                                return (cb());
+                        });
+                },
+
+                function updateMoray(_, cb) {
+                        LOG.debug({
+                                bucket: opts.bucket,
+                                key: key,
+                                data: status
+                        }, 'Writing current status');
+                        client.put(opts.bucket, key, status, function (err) {
+                                if (err) {
+                                        cb(err);
+                                } else {
+                                        LOG.info({
+                                                bucket: opts.bucket,
+                                                key: key,
+                                                status: status
+                                        }, 'status updated');
+                                        cb();
+                                }
+                        });
+                }
+        ] }, function (err) {
+                if (err)
+                        errorAndExit(err, 'failed to update moray');
+
+        });
+}
+
+
+///--- Mainline
+
+// Because everyone asks, the vars here are prefixed with '_' to not consume
+// the global namespace, and then cause scope errors from javascriptlint
+// in functions.
+
+var _cfg = readConfig();
+var _bucket = _cfg.moray.bucket.name;
+var _client = moray.createClient({
+        connectTimeout: _cfg.moray.connectTimeout,
+        log: LOG,
+        url: _cfg.moray.url
+});
+var _schema = {
+        schema: _cfg.moray.bucket.schema
+};
+// Guarantee the bucket exists, then just schedule the runner
+_client.putBucket(_bucket, _schema, function (err) {
+        if (err)
+                errorAndExit(err, 'unable to putBucket');
+
+        LOG.info({
+                bucket: _bucket,
+                objectRoot: _cfg.objectRoot
+        }, 'Moray bucket Ok. Starting stat daemon');
+        setInterval(function heartbeat() {
+                var opts = {
+                        bucket: _bucket,
+                        client: _client,
+                        domain: _cfg.domain,
+                        objectRoot: _cfg.objectRoot
+                };
+                run(opts);
+        }, (_cfg.interval || 5000));
+});
